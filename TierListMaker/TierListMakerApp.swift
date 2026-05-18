7a97b82d97dad157fe0ea120937e4b25f394ac3c
Kanya TOME <e245760@ie.u-ryukyu.ac.jp>
2026-05-18 22:35:00 +0900

    Initial Commit

---
+++ TierListMaker/TierListMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TierListMakerApp.swift
+//  TierListMaker
+//
+//  Created by Tome Kanya   on 2026/05/18.
+//
+
+import SwiftUI
+
+@main
+struct TierListMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
